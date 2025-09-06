340cdd6019d5c27eec579665ce4ccc3c5e138b54
meneer-van-informatica <dbn@fioretti.nl>
2025-09-06 20:01:52 +0200

    greet: yo maatje

--- blink.cmd
+++ blink.cmd
@@ -1,2 +1,5 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\hue_blink_all.ps1" -Beat %*
+setlocal
+set "ARGS=%*"
+echo %ARGS% | findstr /I "\-Beat" >nul || set "ARGS=-Beat %ARGS%"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\hue_blink_all.ps1" %ARGS%
