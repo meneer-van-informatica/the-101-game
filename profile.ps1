5d58a3c298355ad0f612ba99318cb155b400a411
meneer-van-informatica <dbn@fioretti.nl>
2025-09-06 21:32:52 +0200

    profile: softclear (cls) before every bleep/blink/bloop; dot-source from $PROFILE

--- profile.ps1
+++ profile.ps1
@@ -1,16 +1,25 @@
 ﻿$root='E:\the-101-game'
 $exe="$root\.venv\Scripts"
 
-Remove-Item alias:blink,alias:bleep,alias:bloop,function:blink,function:bleep,function:bloop,function:Invoke-SoftClear,alias:cls -ErrorAction SilentlyContinue
-
-function Invoke-Chain{param([string]$name) if(Test-Path ".\$name"){& ".\$name" @args;return} if(Test-Path "$exe\$name.exe"){& "$exe\$name.exe" @args;return} Write-Host "[err] $name niet gevonden"}
-
-function export{(Get-History|Format-List|Out-String)|Set-Clipboard;Write-Host '[ok] export → clipboard'}
-
-function blink{1..5 | ForEach-Object { .\blink }}
-function bleep{Invoke-Chain 'bleep'}
-function bloop{Invoke-Chain 'bloop'}
-
 function Invoke-SoftClear{Clear-Host;Write-Host '[ok] softclear'}
 Set-Alias cls Invoke-SoftClear -Force
-# let 'sc' met rust (AllScope in Windows PowerShell)
+
+function bleep{
+  Invoke-SoftClear
+  if(Test-Path .\bleep){& .\bleep}
+  elseif(Test-Path "$exe\bleep.exe"){& "$exe\bleep.exe"}
+  else{[console]::beep(1000,200);Write-Host '[ok] bleep (alert) klaar.'}
+}
+
+function blink{
+  Invoke-SoftClear
+  1..5 | ForEach-Object { .\blink }
+}
+
+function bloop{
+  Invoke-SoftClear
+  bleep
+  blink
+  bleep
+  blink
+}
